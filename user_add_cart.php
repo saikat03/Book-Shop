9ed148838d47660eda9b95e61e5158503c81d53f
sourav078 <80679618+sourav078@users.noreply.github.com>
2021-06-05 01:57:09 +0600

    Add files via upload

---
+++ user_add_cart.php
@@ -0,0 +1,10 @@
+<?php
+    session_start();
+
+    if (!isset($_SESSION['id']) ||(trim ($_SESSION['id']) == '')) {
+        header('location:user_login_page.php');
+        exit();
+    }
+    
+?>
+                        
